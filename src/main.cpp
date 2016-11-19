35854ca11de7df50d251abe9ae9d1d5ca7addee7
Hunter Damron <hdamron1594@yahoo.com>
2016-11-18 20:46:18 -0500

    Reverted to before committing from codeshare. Functional minus debugging

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,12 @@ int main() {
     simple <<
             "10 input x\n"
             "12 input y\n"
-            "12 goto 20\n"
+            "13 if x > y goto 20\n"
             "14 rem hello steven\n"
+            "15 let z = y + x * 4\n"
             "20 output x\n"
             "21 end\n";
-  
-    compiler::compile("simple.smp", "compiled.sml");
+    
+//    compiler::compile("simple.smp", "compiled.sml");
+    compiler::compile(&simple, &cout);
 }
