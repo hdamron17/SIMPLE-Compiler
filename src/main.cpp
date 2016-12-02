fd30e28f607d1f057f1486dd9f4b992d457fc92d
Hunter Damron <hdamron1594@yahoo.com>
2016-12-02 10:34:47 -0500

    Version from Codeshare. Most updated version.

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,5 @@ int main() {
             "20 output x\n"
             "21 end\n";
     
-//    compiler::compile(&simple, &cout);
-    compiler cpl;
-    vector<string> testvec = vector<string>{"24", "let", "z", "=", "y", "+", "x", "*", "4", "*", "(", "2", "+", "y", ")"};
-    tuple<string,int> test = cpl.let(&testvec);
-    cout << get<0>(test) << endl;
+    compiler::compile(&simple, &cout);
 }
