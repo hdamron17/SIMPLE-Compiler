f0e11595e9cf9bed3c431690998675d26381c280
Hunter Damron <hdamron1594@yahoo.com>
2016-12-02 10:54:18 -0500

    It works, but needs to be mixed with Brennan's changes from Codeshare.

--- src/main.cpp
+++ src/main.cpp
@@ -16,5 +16,10 @@ int main() {
             "15 let x = y + x * 4 * ( 2 + y )\n"
             "20 output x\n"
             "21 end\n";
+    
     compiler::compile(&simple, &cout);
+//    compiler cpl;
+//    vector<string> testvec = vector<string>{"24", "let", "z", "=", "y", "+", "x", "*", "4", "*", "(", "2", "+", "y", ")"};
+//    tuple<string,int> test = cpl.let(&testvec);
+//    cout << get<0>(test) << endl;
 }
